e6df34fa64983e638e01c440fc69e946fda85b51
ardaereny <ardaerenyilmaz@hotmail.com>
2024-07-11 16:46:21 +0300

    problem solving (appendAndDelete,repeatedString)

--- src/main/java/com/ardaeren/hackerrank/HackerrankApplication.java
+++ src/main/java/com/ardaeren/hackerrank/HackerrankApplication.java
@@ -3,6 +3,11 @@ package com.ardaeren.hackerrank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class HackerrankApplication {
 
@@ -33,6 +38,33 @@ public class HackerrankApplication {
             }
         }
         return "No";
+    }
+
+    public static long repeatedString(String s, long n){
+        // 2 durum var 1 -> string size ile n size birbirini katıysa
+        // stringdeki en çok tekrar eden sayının ne kadar geçtiğini bul
+        //  n/s.length kadar o sayıyı çarp
+        // 2-> string size ile n birbirinin katı değilse
+        // üssteki işlemleri yap + olarak n%s kalanını da contains...
+        long ekstraAsayisi;
+        long aTekrarSayisi;
+
+        List<Character> charList = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            charList.add(c);
+        }
+        aTekrarSayisi = charList.stream().filter(ch -> ch == 'a').count();
+
+
+        if(n%s.length() == 0){
+            return n/s.length() * aTekrarSayisi;
+        }
+        else{
+            var ekstraharfler = n % s.length();
+            var kalankelime = s.substring(0,(int) ekstraharfler);
+            ekstraAsayisi = kalankelime.chars().filter(ch -> ch == 'a').count();
+            return ekstraAsayisi + n/s.length() * aTekrarSayisi;
+        }
     }
 
 }
